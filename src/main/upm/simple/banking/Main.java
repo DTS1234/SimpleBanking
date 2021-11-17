378949925cbc0676ba8d694637508854a89020f9
krissiey97 <krissiey1997@gmail.com>
2021-11-17 19:45:26 +0100

    Addmoney initial

--- src/main/upm/simple/banking/Main.java
+++ src/main/upm/simple/banking/Main.java
@@ -3,7 +3,7 @@ package main.upm.simple.banking;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 
 }
--- src/main/upm/simple/banking/logic/account/AddMoneyCommand.java
+++ src/main/upm/simple/banking/logic/account/AddMoneyCommand.java
@@ -1,16 +1,25 @@
 package main.upm.simple.banking.logic.account;
 
+import main.upm.simple.banking.persistance.AccountRepository;
+
 /**
  * @author akazmierczak
  * @create 14.11.2021
  */
 public class AddMoneyCommand {
 
+    private AccountRepository accountRepository;
+
+    public AddMoneyCommand(AccountRepository accountRepositoryInput) {
+        this.accountRepository = accountRepositoryInput;
+    }
     /**
      * This command adds the specified amount to the specified account and notifies the
      * user afterwards that the addition has been successful. Furthermore, the previous and
      * the new balance are displayed.
      */
+
+
     void execute() {
 
     }
